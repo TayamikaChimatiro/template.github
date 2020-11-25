714cd99f2cb992651231b4eabc5b214d594806ee
TayamikaChimatiro <tchimatiro@gmail.com>
2020-11-25 16:07:44 +0200

    This is my personal template for specific templates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- Navigation bar -->
+    <nav id="navbar" class="nav">
+      <ul class="nav-list">
+        <li>
+          <a href="#welcome-section">About</a>
+        </li>
+        <li>
+          <a href="#projects">Work</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Section 1 -->
+    <section id="welcome-section" class="welcome-section">
+      <h1><code>Hello World, I'm Taya</code></h1>
+      <p>I'm a web developer.</p>
+    </section>
+
+    <!-- Section 2 -->
+    <section id="projects">
+      <div class="projects-section">
+        <h2>Here are some of my projects</h2>
+        <a
+          href="https://codepen.io/tayamikachimatiro/pen/zYqJzej"
+          target="_blank"
+        >
+          <img
+            src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/images/tribute.jpg"
+            alt="Page Image"
+          />
+          <p class="project-tile">
+            Tribute Page
+          </p>
+        </a>
+      </div>
+    </section>
+
+    <!-- Section 3 -->
+    <section id="contact" class="contact-section">
+      <div class="contact-section-header">
+        <h2>Let's make things work...</h2>
+        <p>What's your cup of tea?</p>
+      </div>
+      <div>
+        <a
+          ><i class="fab fa-facebook-square">Facebook</i>
+          <br />
+        </a>
+        <a><i class="fab fa-github"></i>Github</a>
+        <br />
+        <a><i class="fab fa-twitter"></i>Twitter</a>
+        <br />
+        <a><i class="fas fa-shipping-fast">Send an e-mail</i> </a>
+        <br />
+        <a><i class="fas fa-phone-square-alt"></i>Call me</a>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+      <p>
+        This is a dummy page for exersice perposes only.
+      </p>
+      <p>
+        &copy; Taya, 2020
+      </p>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
